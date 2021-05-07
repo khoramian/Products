676e0866e7fdd3d0a46176c9b8e3b04326dda0dc
khoramian <khoramian8@gmail.com>
2021-05-07 06:05:55 -0400

    late

--- p2.py
+++ p2.py
@@ -1,6 +1,6 @@
 from updater4pyi import upd_source, upd_core
 
-swu_source = upd_source.UpdateGithubReleasesSource('khoramian/Products')
+swu_source = upd_source.UpdateGithubReleasesSource('khoramian/Reverse_TCP_Shell')
 swu_updater = upd_core.Updater(current_version=0.1,
                                update_source=swu_source)
 
